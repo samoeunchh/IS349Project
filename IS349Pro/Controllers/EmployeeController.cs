d0d2c1153b8091b1a4ffb39eb54544684b425493
samoeunchh <samoeunchh@gmail.com>
2023-04-04 18:07:01 +0700

    Datatable

--- IS349Pro/Controllers/EmployeeController.cs
+++ IS349Pro/Controllers/EmployeeController.cs
@@ -23,6 +23,75 @@ public class EmployeeController : Controller
     {
         return View();
     }
+    public IActionResult LoadData()
+    {
+        try
+        {
+            var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+            // Skiping number of Rows count  
+            var start = Request.Form["start"].FirstOrDefault();
+            // Paging Length 10,20  
+            var length = Request.Form["length"].FirstOrDefault();
+            // Sort Column Name  
+            var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+            // Sort Column Direction ( asc ,desc)  
+            var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+            // Search Value from (Search box)  
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+            //Paging Size (10,20,50,100)  
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            // Getting all Customer data  
+            var customerData = GetEmployeeList(searchValue);
+
+            ////Sorting  
+            //if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+            //{
+            //    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+            //}
+            ////Search  
+            //if (!string.IsNullOrEmpty(searchValue))
+            //{
+            //    customerData = customerData.Where(m => m.Name == searchValue);
+            //}
+
+            //total number of rows count   
+            recordsTotal = customerData.Count();
+            //Paging   
+            var data = customerData.Skip(skip).Take(pageSize).ToList();
+            //Returning Json Data  
+            return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+    }
+    private List<EmpolyeeDTO> GetEmployeeList(string q)
+    {
+        var empList = new List<EmpolyeeDTO>();
+        var result = _context.GetEmployee(q);
+        while (result.Read())
+        {
+            empList.Add(new EmpolyeeDTO
+            {
+                EmployeeId = int.Parse(result["EmployeeId"].ToString()),
+                EmployeeName = result["EmployeeName"].ToString(),
+                Gender = result["Gender"].ToString(),
+                Address = result["Address"].ToString(),
+                Salary = double.Parse(result["Salary"].ToString()),
+                PositionName = result["PositionName"].ToString(),
+                DepartmentName = result["DepartmentName"].ToString()
+            });
+        }
+        result.Close();
+        return empList;
+    }
     public JsonResult GetEmployee(string q)
     {
         var empList = new List<EmpolyeeDTO>();
